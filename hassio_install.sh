09f21efb825b532733adacc3f0683f5056e6b64c
Mike Balmer <remlabm@users.noreply.github.com>
2019-07-27 17:04:38 -0700

    Update hassio_install.sh

--- hassio_install.sh
+++ hassio_install.sh
@@ -80,7 +80,7 @@ case $ARCH in
         HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
         HASSIO_DOCKER="$DOCKER_REPO/amd64-hassio-supervisor"
     ;;
-    "arm" |"armv6l")
+    "arm" |"armv6l" |"armv7l")
         if [ -z $MACHINE ]; then
             echo "[ERROR] Please set machine for $ARCH"
             exit 1
@@ -88,14 +88,14 @@ case $ARCH in
         HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
         HASSIO_DOCKER="$DOCKER_REPO/armhf-hassio-supervisor"
     ;;
-    "armv7l")
-        if [ -z $MACHINE ]; then
-            echo "[ERROR] Please set machine for $ARCH"
-            exit 1
-        fi
-        HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
-        HASSIO_DOCKER="$DOCKER_REPO/armv7-hassio-supervisor"
-    ;;
+#     "armv7l")
+#         if [ -z $MACHINE ]; then
+#             echo "[ERROR] Please set machine for $ARCH"
+#             exit 1
+#         fi
+#         HOMEASSISTANT_DOCKER="$DOCKER_REPO/$MACHINE-homeassistant"
+#         HASSIO_DOCKER="$DOCKER_REPO/armv7-hassio-supervisor"
+#     ;;
     "aarch64")
         if [ -z $MACHINE ]; then
             echo "[ERROR] Please set machine for $ARCH"
